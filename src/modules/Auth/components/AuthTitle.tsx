196dec9e69d6d125649d6c2eadcb05b318ee4bf0
Alexander Burachkov <zicio@yandex.ru>
2023-08-08 23:29:13 +0400

    Fixed auth layout

--- src/modules/Auth/components/AuthTitle.tsx
+++ src/modules/Auth/components/AuthTitle.tsx
@@ -1,26 +1,54 @@
 "use client";
 
-import { useContext } from "react";
+import { useContext, useEffect, useId } from "react";
 import { AuthContext, AuthTypes } from "@/modules/Auth/Auth";
 import styles from "../Auth.module.scss";
 
 const AuthTitle = () => {
-  const { setAuthType } = useContext(AuthContext);
+  const toBeSelectedId = useId();
+  const { authType, setAuthType } = useContext(AuthContext);
+
+  //  Костыль для отображения первоначального положения радио кнопок. В Next JS with App Router c этим пока проблема https://github.com/vercel/next.js/issues/49499
+  useEffect(() => {
+    const checkedInput: HTMLInputElement | null = document.querySelector(
+      `[data-id="${toBeSelectedId}"]`,
+    );
+    if (checkedInput) {
+      checkedInput.checked = true;
+    }
+  }, [toBeSelectedId]);
+
   return (
     <h1 className={styles.auth__title}>
-      <button
-        className={styles.auth__typeButton}
-        onClick={() => setAuthType(AuthTypes.signIn)}
-      >
+      <label htmlFor="signIn">
+        <input
+          className={styles.auth__input}
+          type="radio"
+          name="authType"
+          id="signIn"
+          value={AuthTypes.signIn}
+          checked={authType === AuthTypes.signIn}
+          onChange={(event) => setAuthType(event.target.value as AuthTypes)}
+          data-id={toBeSelectedId}
+        />
+        <span className={styles.auth__customInput}></span>
         Войти
-      </button>
+      </label>
       {" или "}
-      <button
-        className={styles.auth__typeButton}
-        onClick={() => setAuthType(AuthTypes.signUp)}
-      >
+
+      <label htmlFor="signUp">
+        <input
+          className={styles.auth__input}
+          type="radio"
+          name="authType"
+          id="signUp"
+          value={AuthTypes.signUp}
+          checked={authType === AuthTypes.signUp}
+          onChange={(event) => setAuthType(event.target.value as AuthTypes)}
+        />
+        <span className={styles.auth__customInput}></span>
         Зарегистрироваться
-      </button>
+      </label>
     </h1>
   );
 };
